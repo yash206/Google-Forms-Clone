e5ec3f3f65544c9e0ae4189c245f0cc26f2365e8
yash agarwal <agarwalyash9120076911@gmail.com>
2023-05-10 03:00:59 +0530

    Modified

--- GoogleForms/Forms/urls.py
+++ GoogleForms/Forms/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'Forms'
+app_name = 'forms'
 
 urlpatterns = [
-    path('', views.home,name = 'home'),
+    path('', views.home,name = 'home',),
     path('create/', views.create, name='create'),
     path('forms/<str:pk>/edit', views.update_form, name='update_form'),
     path('response/<str:pk>/viewForm', views.get_response_from_user, name='view_form'),
